4a24bb28054adb048ee4ef31c384a60b21314da5
Salim Nassim <salim.nassim@gmail.com>
2023-08-11 13:56:25 -0400

    torrents can be larger then int, use bigint

---
+++ migrations/20230811174303_int_to_bigint.down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE public.peers ALTER COLUMN uploaded TYPE integer;
+ALTER TABLE public.peers ALTER COLUMN downloaded TYPE integer;
+ALTER TABLE public.peers ALTER COLUMN "left" TYPE integer;
