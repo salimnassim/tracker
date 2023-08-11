20783f17a8452f35e77be149761d4e78fcb86153
Salim Nassim <salim.nassim@gmail.com>
2023-08-11 13:56:34 -0400

    add table for announce log

---
+++ migrations/20230811175535_announce_log.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS public.announce_log
+(
+    id uuid NOT NULL,
+    info_hash bytea NOT NULL,
+    peer_id bytea NOT NULL,
+    event text COLLATE pg_catalog."default" NOT NULL,
+    ip inet NOT NULL,
+    port integer NOT NULL,
+    key text COLLATE pg_catalog."default" NOT NULL,
+    uploaded bigint NOT NULL,
+    downloaded bigint NOT NULL,
+    "left" bigint NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    CONSTRAINT announce_log_pkey PRIMARY KEY (id)
+)
+
+ALTER TABLE IF EXISTS public.announce_log
+    OWNER to tracker;
